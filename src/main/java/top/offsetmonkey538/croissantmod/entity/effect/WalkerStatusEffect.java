73ed975a316623b6484520129434a0ac578f335a
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-08-01 18:10:41 +0300

    Make the walker status effects more like vanilla frost walker

--- src/main/java/top/offsetmonkey538/croissantmod/entity/effect/WalkerStatusEffect.java
+++ src/main/java/top/offsetmonkey538/croissantmod/entity/effect/WalkerStatusEffect.java
@@ -1,6 +1,10 @@
 package top.offsetmonkey538.croissantmod.entity.effect;
 
-import net.minecraft.block.*;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.FluidBlock;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.enchantment.FrostWalkerEnchantment;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
@@ -27,6 +31,11 @@ public class WalkerStatusEffect extends StatusEffect {
 
     @Override
     public void applyUpdateEffect(LivingEntity entity, int amplifier) {
+        // The amplifier for status effects starts from 0
+        // whereas the one for enchantments starts from 1.
+        // That's why we need to increment the amplifier by 1.
+        amplifier++;
+
         // Compatibility with my better frost walker mod
         // by using the vanilla code for freezing water.
         if (this.walkOn == Blocks.WATER && this.convertTo == Blocks.FROSTED_ICE) {
@@ -46,7 +55,7 @@ public class WalkerStatusEffect extends StatusEffect {
         for (BlockPos iteratedPos : BlockPos.iterate(blockPos.add(-area, -1, -area), blockPos.add(area, -1, area))) {
             final BlockState iteratedBlock = world.getBlockState(iteratedPos);
 
-            if (!iteratedPos.isWithinDistance(entity.getPos(), area) || !iteratedBlock.isOf(this.walkOn) || !blockState.canPlaceAt(world, iteratedPos) || !world.canPlace(blockState, iteratedPos, ShapeContext.absent())) continue;
+            if (!iteratedPos.isWithinDistance(entity.getPos(), area) || !iteratedBlock.isOf(this.walkOn) || !blockState.canPlaceAt(world, iteratedPos) || !world.canPlace(blockState, iteratedPos, ShapeContext.absent()) || iteratedBlock.get(FluidBlock.LEVEL) != 0) continue;
 
 
             world.setBlockState(iteratedPos, blockState);
