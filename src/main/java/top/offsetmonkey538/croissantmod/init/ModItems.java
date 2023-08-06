5c169619b3b41e2c1c9205ed9ffc22436e1c17e7
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-08-06 12:51:45 +0300

    Add croissants to creative inventory

--- src/main/java/top/offsetmonkey538/croissantmod/init/ModItems.java
+++ src/main/java/top/offsetmonkey538/croissantmod/init/ModItems.java
@@ -1,6 +1,9 @@
 package top.offsetmonkey538.croissantmod.init;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import top.offsetmonkey538.croissantmod.item.CroissantItem;
@@ -22,7 +25,14 @@ public final class ModItems {
         return Registry.register(Registries.ITEM, id(name), item);
     }
 
+    @SuppressWarnings("UnstableApiUsage")
     public static void register() {
         // Registers items by loading the class
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
+            entries.addAfter(Items.BREAD, CROISSANT);
+            entries.addAfter(CROISSANT, ICE_CROISSANT);
+            entries.addAfter(ICE_CROISSANT, INFERNAL_CROISSANT);
+        });
     }
 }
